50fec6f4d323c463f94ac70520a8c037642f93da
umi <umi@apricot.(none)>
2012-12-02 16:06:43 +0400

    Bugfixes for the script

--- utils/clean_table.sh
+++ utils/clean_table.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+    echo "Usage: clean_table.sh number"
+    exit
+fi
+
+set -x
 NUM_TO_KEEP=$1
-NUM_TO_DELETE=`mysql -u$NOVA_USERNAME -p$NOVA_PASSWORD --skip-column-names -s vmstats -e "SELECT COUNT(*) - 2 from vmpcpustats);"`
-mysql -u$NOVA_USERNAME -p$NOVA_PASSWORD vmstats -e "DELETE FROM vmpcpustats ORDER BY date LIMIT '$NUM_TO_DELETE';"
+NUM_TO_DELETE=`mysql -u$NOVA_USERNAME -p$NOVA_PASSWORD --skip-column-names -s vmstats -e "SELECT COUNT(*) - $NUM_TO_KEEP from vmpcpustats;"`
+mysql -u$NOVA_USERNAME -p$NOVA_PASSWORD vmstats -e "DELETE FROM vmpcpustats ORDER BY date LIMIT $NUM_TO_DELETE;"
 mysql -u$NOVA_USERNAME -p$NOVA_PASSWORD vmstats -e "SELECT * FROM vmpcpustats;"
